3e616720a5b90aa1b3608559a3fd7718c2813220
CasselKim <seongr39@gmail.com>
2022-02-08 23:45:54 +0900

    Update ticker lists

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -9,7 +9,8 @@ def index(request) :
         print("하이하이", get_user(request))
         user = UserInfo.objects.get(id=get_user(request))
         account_state = AccountState.objects.get(userid=user)
-        context = {'account_state' : account_state}
+        trade_per_coin = TradePerCoin.objects.filter(userid=user)
+        context = {'account_state' : account_state, 'trade_per_coin':trade_per_coin}
         return render(request, 'polls/manage.html', context)
     else : 
         return redirect('common/login')
